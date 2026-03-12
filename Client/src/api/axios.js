2a97466f08d40fda9375d42cc341dfd384effa73
Vishal Pal <vishal.pal111283@gmail.com>
2026-03-12 17:10:39 +0530

    update backend api url

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DEFAULT_API_BASE_URL = "http://127.0.0.1:5002/api";
+const DEFAULT_API_BASE_URL = "https://google-signup.onrender.com/api";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || DEFAULT_API_BASE_URL,
