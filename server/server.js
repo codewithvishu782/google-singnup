5c84bb6a04ce25fdb4a5788e2056565febf1b75c
Vishal Pal <vishal.pal111283@gmail.com>
2026-03-12 17:18:14 +0530

    fix cors and api url

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
   res.send("Server Running Successfully 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
